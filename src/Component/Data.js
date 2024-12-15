e7d4335b18fa388c5e0f27e826be8175d67f8fba
Manish Rouniyar <manish.rouniyar2060@gmail.com>
2024-12-15 13:05:06 +0530

    Third Push

---
+++ src/Component/Data.js
@@ -0,0 +1,164 @@
+export const Data = [
+    // Existing questions
+    {
+        q: "What is JavaScript?",
+        a: "JS is Programming language",
+        b: "JS is Framework",
+        c: "None",
+        d: "Typed Language",
+        ans: "JS is Programming language"
+    },
+    {
+        q: "What is ReactJS?",
+        a: "Library",
+        b: "React is a JavaScript Library",
+        c: "Framework",
+        d: "Language",
+        ans: "React is a JavaScript Library"
+    },
+    {
+        q: "Full Form of JS?",
+        a: "JavaSell",
+        b: "JavaSpring",
+        c: "JavaScript",
+        d: "JavaSplit",
+        ans: "JavaScript"
+    },
+    {
+        q: "Who Developed JS?",
+        a: "Brendon",
+        b: "Brendon Mac",
+        c: "Bretly",
+        d: "Brendon Eich",
+        ans: "Brendon Eich"
+    },
+    {
+        q: "Who Developed ReactJS?",
+        a: "Jordan Walk",
+        b: "Jordan",
+        c: "Jordan King",
+        d: "Jordan Sen",
+        ans: "Jordan Walk"
+    },
+    // Additional questions
+    {
+        q: "Which company maintains ReactJS?",
+        a: "Google",
+        b: "Facebook",
+        c: "Apple",
+        d: "Microsoft",
+        ans: "Facebook"
+    },
+    {
+        q: "Which of the following is not a JavaScript data type?",
+        a: "Number",
+        b: "Boolean",
+        c: "String",
+        d: "Float",
+        ans: "Float"
+    },
+    {
+        q: "What is JSX in React?",
+        a: "A JavaScript extension",
+        b: "A CSS framework",
+        c: "A database",
+        d: "A JavaScript library",
+        ans: "A JavaScript extension"
+    },
+    {
+        q: "Which of these is a JavaScript framework?",
+        a: "Django",
+        b: "React",
+        c: "Spring",
+        d: "Flask",
+        ans: "React"
+    },
+    {
+        q: "Which keyword is used to declare a variable in JavaScript?",
+        a: "let",
+        b: "var",
+        c: "const",
+        d: "All of the above",
+        ans: "All of the above"
+    },
+    {
+        q: "What does DOM stand for?",
+        a: "Document Object Model",
+        b: "Data Object Model",
+        c: "Document Operational Model",
+        d: "Data Operational Method",
+        ans: "Document Object Model"
+    },
+    {
+        q: "Which method is used to fetch data in React?",
+        a: "getData()",
+        b: "fetch()",
+        c: "useEffect()",
+        d: "React.useData()",
+        ans: "fetch()"
+    },
+    {
+        q: "Which lifecycle method runs after a component is updated?",
+        a: "componentDidMount",
+        b: "componentDidUpdate",
+        c: "shouldComponentUpdate",
+        d: "render",
+        ans: "componentDidUpdate"
+    },
+    {
+        q: "Which of the following is a higher-order function in JavaScript?",
+        a: "map",
+        b: "filter",
+        c: "reduce",
+        d: "All of the above",
+        ans: "All of the above"
+    },
+    {
+        q: "What is the default port for a React application?",
+        a: "3000",
+        b: "8000",
+        c: "5000",
+        d: "8080",
+        ans: "3000"
+    },
+    {
+        q: "Which hook is used to manage state in functional components?",
+        a: "useContext",
+        b: "useReducer",
+        c: "useState",
+        d: "useEffect",
+        ans: "useState"
+    },
+    {
+        q: "What is the purpose of React Router?",
+        a: "To create animations",
+        b: "To manage API calls",
+        c: "To handle routing in a React application",
+        d: "To manage state",
+        ans: "To handle routing in a React application"
+    },
+    {
+        q: "Which of the following is true about JavaScript?",
+        a: "It is a strongly typed language",
+        b: "It is an interpreted language",
+        c: "It is a compiled language",
+        d: "None of the above",
+        ans: "It is an interpreted language"
+    },
+    {
+        q: "What is the use of the `this` keyword in JavaScript?",
+        a: "To access the current object",
+        b: "To declare variables",
+        c: "To call a function",
+        d: "None of the above",
+        ans: "To access the current object"
+    },
+    {
+        q: "Which of the following is used for styling in React?",
+        a: "CSS",
+        b: "Inline styles",
+        c: "CSS-in-JS libraries",
+        d: "All of the above",
+        ans: "All of the above"
+    }
+];
